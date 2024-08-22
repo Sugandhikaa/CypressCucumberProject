29e80fbc348fa305d40cec0a63a9deda5770b850
sugandhikaa <sodonline2@gmail.com>
2024-08-23 03:38:52 +0530

    Varibale add as a dataset

--- cypress/e2e/features/board.feature
+++ cypress/e2e/features/board.feature
@@ -3,4 +3,6 @@ Feature: Board functionality
   Scenario: Create a board
     Given I am on empty home page
     When I type and submit in the board name
+    |UserName|password|
+    |student|Password123|
     Then I should be redirected to the board detail
--- cypress/e2e/step_definitions/board.js
+++ cypress/e2e/step_definitions/board.js
@@ -4,11 +4,14 @@ Given("I am on empty home page", () => {
   cy.visit("https://practicetestautomation.com/practice-test-login/");
 });
 
-When("I type and submit in the board name", () => {
-  cy.get("#username").type('student');
-  cy.get("#password").type("Password123");
-  cy.get("#submit").click();
-});
+When("I type and submit in the board name", (dataTable) => {
+  dataTable.hashes().forEach((element) => {
+    cy.get("#username").type(element.UserName);
+    cy.get("#password").type(element.password);
+    cy.get("#submit").click();
+  });});
+ 
+
 
 Then("I should be redirected to the board detail", () => {
     cy.get(".post-title");
